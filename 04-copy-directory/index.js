2efd87c12e6aa828d9fe4a7b5463c28e4ff0b089
denisselet <96264844+denisselet@users.noreply.github.com>
2022-05-22 20:38:24 +0300

    feat: 04 task completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
 
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+fs.readdir(path.join(__dirname, 'files'), 
+    { withFileTypes: true },
+    (err, files) => {
+        files.forEach(file => {
+            if (file.isFile() === true) {
+                const files = path.join(__dirname, 'files', file.name)
+                const files_copy = path.join(__dirname, 'files-copy', file.name)
+                fsPromises.copyFile(files, files_copy)
+            }
+        })
+    }
+)
+
+  fs.readdir(path.join(__dirname, 'files-copy'), 
+  { withFileTypes: true },
+  (err, files) => {
+      files.forEach(file => {
+          if (file.isFile() === true) {
+            fs.access(path.join(__dirname, 'files', file.name), fs.constants.F_OK, (err) => {
+                if (err) {
+                    fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {});
+                }
+              });
+          }
+      })
+  }
+)
